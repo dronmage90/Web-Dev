de354bce791b7216b290de74e0b973cbe3bc68e3
Andrew <dronmage90@gmail.com>
2023-02-22 22:00:01 +0600

    Lab 4 - Edited added

---
+++ Lab 4/Kaspi Market/README.md
@@ -0,0 +1,8 @@
+# Kaspi
+
+KBTU 2022-2023 `Web-Dev Lab4`
+
+## Run
+
+cmd - > `ng serve` -> `http://localhost:4200/`
+
---
+++ Lab 4/Kaspi Market/src/app/products.ts
@@ -0,0 +1,45 @@
+export const products=[{
+
+    name:'Apple iPhone 13 128Gb черный',
+    description:'Apple iPhone 13 получил дисплей 6.1 дюйма Super Retina XDR, который отличается невероятно высокой плотностью пикселей — фотографии, видео и текст выглядят поразительно четко. ',
+    link:"https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000",
+    imgURL:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hc2/h05/46392662458398/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    rating:5,
+    url:"https://t.me/krakozabrya1000/8"
+    }    ,
+
+    {
+        name:'Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+        description:'технология NFC Откройте для себя больше возможностей',
+        link:"https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000",
+        imgURL:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h30/hd2/64487157760030/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+        rating:5,
+        url:"https://t.me/krakozabrya1000/22"
+    }
+    ,
+    {
+        name:'Apple iPhone 14 128Gb голубой',
+        description:'Apple iPhone 14',
+        link:"https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000",
+        imgURL:'https://resources.cdn-kaspi.kz/img/m/p/hbc/h02/64481569701918.jpg?format=preview-medium',
+        rating:5,
+        url:"https://t.me/krakozabrya1000/19"
+    },
+  {
+    name:'Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    description:'технология NFC: Да\nцвет: серый\nтип экрана: цветной AMOLED, сенсорный',
+    link:"https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000",
+    imgURL:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h30/hd2/64487157760030/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+    rating:5,
+    url:"https://t.me/krakozabrya1000/7"
+  },
+  {
+    name:'Apple iPhone 14 Pro 256Gb фиолетовый',
+    description:' iPhone 14 Pro 256Gb',
+    link:"https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000",
+    imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h7b/hdc/64428132991006.jpg?format=preview-medium',
+    rating:5,
+    url:"https://t.me/krakozabrya1000/20"
+  }
+
+]
